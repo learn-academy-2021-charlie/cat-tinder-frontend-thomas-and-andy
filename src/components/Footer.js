8ef1086280eb5b003e8318f6bc063efcb67cc47f
Andy19936 <andy19936@gmail.com>
2021-08-23 23:04:07 -0700

    small update to header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-
+import { GiDrippingHoney } from "react-icons/gi";
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <p>@ Andy & Thomas👀</p>
+        {/* <img className ='header-logo'src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIzQ7EFJn3ieAbyYylCNhAI1SxS6sVkTLerw&usqp=CAU"}/> */}
+        <GiDrippingHoney color='purple' size='8rem' opacity='.7'/>
+        <h3 >© Andy & Thomas</h3>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { MdWork } from "react-icons/md";
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <p>header</p>
+        <MdWork color='#00FF72' size='5rem' opacity='0.6'/>
+        <h4>DevMatch</h4>
       </div>
     );
   }
