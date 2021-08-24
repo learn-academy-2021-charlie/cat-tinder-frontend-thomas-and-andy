8c2ffffc18455fe8aa127df2b8c12ae23770fa52
oddtomas <oddthomas@csu.fullerton.edu>
2021-08-24 16:39:36 -0700

    added nav bar, cleaned up styling, and added card images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { MdWork } from "react-icons/md";
+import NavBar from "./NavBar";
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
+        <NavBar />
         <MdWork color="#00FF72" size="5rem" opacity="0.6" />
         <h4>DevMatch</h4>
       </div>
--- src/pages/DevEdit.js
+++ src/pages/DevEdit.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 class DevEdit extends Component {
   render() {
     return (
-      <>
+      <div className="page-body">
         <h3>This is the Edit</h3>
-      </>
+      </div>
     );
   }
 }
